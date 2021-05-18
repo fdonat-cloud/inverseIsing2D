00aac26d06a43d6798224b0ecfb74d26b7ef05ed
Federico Donat <federico.donat@studio.unibo.it>
2021-05-18 10:06:57 +0200

    started with getting the size of the data, putting some constraints, specifying the initial point of optimization

--- inverse_ising.m
+++ inverse_ising.m
@@ -1,1 +1,9 @@
-...............................
+num_configurations = height(configurations);
+num_spins = width(configurations);
+array1 = configurations';
+solution = zeros(num_spins,num_spins);
+beq=0;
+A=-diag(ones(1,num_spins));
+b=zeros(num_spins,1);
+x0=zeros(1,num_spins);
+options = optimoptions(@fmincon,'Algorithm','sqp','MaxFunEvals',300000);
