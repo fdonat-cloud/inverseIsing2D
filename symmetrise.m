2519792e96bd1b77d543449ad1a66ec523dc6ab5
Federico Donat <federico.donat@studio.unibo.it>
2021-06-13 20:25:39 +0200

    unit tested

---
+++ symmetrise.m
@@ -0,0 +1,35 @@
+function matrix = symmetrise(matrix)
+%SYMMETRISE returns a matrix in which a_ij = a_ji for any i and j
+%   In theory the strength of the n-th spin on the m-th spin is equal to the 
+%   strength of the m-th on the n-th. In practice this is not true due
+%   to sampling errors, so we need to fix this
+
+if not( isnumeric(matrix) )
+    error('imput must be a numeric matrix')
+end
+
+if not(width(matrix) == height(matrix))
+    error('must put a square matrix')
+end
+
+if not( sum(sum(matrix == matrix)) == numel(matrix) )
+    error('input must not have NaN values')
+end
+    
+for i = 1:height(matrix)
+    for j = 1:width(matrix)
+        if j > i
+            matrix(i,j) = (matrix(i,j) + matrix(j,i))/2;
+        end
+    end
+end
+
+for j = 1:width(matrix)
+    for i = 1:height(matrix)
+        if i > j
+            matrix(i,j) = matrix(j,i);
+        end
+    end
+end
+
+end
