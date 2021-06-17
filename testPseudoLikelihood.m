0b979340d04f372c983c40cc224a7c62af78c75d
Federico Donat <federico.donat@studio.unibo.it>
2021-06-17 18:48:49 +0200

    added non-negativity tests: minimal data, degenerate data and/or very large couplings

--- testPseudoLikelihood.m
+++ testPseudoLikelihood.m
@@ -1,7 +1,39 @@
-%%
+%% test function is non negative with minimal data
 couplings = -100;
-configurations = [1];
+configs = [1];
 spin_index = 1;
-num_configurations = height(configurations);
-value = pseudoLikelihood(couplings,configurations,spin_index,num_configurations,configurations');
-assert( value > 0 | value ==0 );
+num_configurations = height(configs);
+value = pseudoLikelihood(couplings,configs,spin_index,num_configurations,configs');
+assert( not (value < 0) );
+
+%% test function is non negative with degenerate data (100 configurations, all +1)
+couplings = zeros(1,100);
+configs = ones(100,100);
+spin_index = 1;
+num_configurations = height(configs);
+value = pseudoLikelihood(couplings,configs,spin_index,num_configurations,configs');
+assert( not (value < 0 ) );
+
+%% test function is non negative with degenerate data (100 configurations, all -1)
+couplings = zeros(1,100);
+configs = -ones(100,100);
+spin_index = 1;
+num_configurations = height(configs);
+value = pseudoLikelihood(couplings,configs,spin_index,num_configurations,configs');
+assert( not (value < 0 ) );
+
+%% test function is non negative with degenerate data and large couplings
+couplings = 1E6 * ones(1,100);
+configs = -ones(100,100);
+spin_index = 1;
+num_configurations = height(configs);
+value = pseudoLikelihood(couplings,configs,spin_index,num_configurations,configs');
+assert( not (value < 0 ) );
+
+%% test function is non negative with degenerate data and large negative couplings
+couplings = -1E6 * ones(1,100);
+configs = -ones(100,100);
+spin_index = 1;
+num_configurations = height(configs);
+value = pseudoLikelihood(couplings,configs,spin_index,num_configurations,configs');
+assert( not (value < 0 ) );
