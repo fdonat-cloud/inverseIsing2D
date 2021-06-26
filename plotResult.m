187b2c5fab4bbc68b277084dde8442cfc954e7cf
Federico Donat <federico.donat@studio.unibo.it>
2021-06-26 15:42:14 +0200

    now it saves the plots in a folder without displaying them

--- plotResult.m
+++ plotResult.m
@@ -1,8 +1,12 @@
+f = figure('visible', 'off');
 plot(couplings,'mo:','LineWidth',2), xlabel('place in the array "couplings"'), ylabel('value'),title('A descent sort of the effective couplings greater than cutoff'), grid on;
-savefig('plot.fig');
-close
+print -djpeg sorting.jpg
+close(f)
+
+g = figure('visible','off');
 histogram(couplings,50), xlabel('value'), ylabel('number of occurrences in the array "couplings"'), grid on, title('Histogram of the effective couplings greater than cutoff');
-savefig('hist.fig');
-close
-figure1 = openfig('plot.fig');
-figure2 = openfig('hist.fig');
+print -djpeg histogram.jpg
+close(g)
+
+movefile('sorting.jpg','plots');
+movefile('histogram.jpg','plots');
