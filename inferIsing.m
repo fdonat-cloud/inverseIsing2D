3927c1122d06219339ded1b7bb699e0b3b116683
Federico Donat <federico.donat@studio.unibo.it>
2021-06-26 11:22:03 +0200

    changed names to some variables for clarity

--- inferIsing.m
+++ inferIsing.m
@@ -1,32 +1,32 @@
 load('default.mat');
-configurations = input('insert the name of the file containing the set of configurations (just press enter if you want to use the default data):');%
+data = input('insert the name of the file containing the set of configurations (just press enter if you want to use the default data):');%
 
-if numel(configurations) == 0
-    configurations = default;
+if numel(data) == 0
+    data = default;
 end
 
-num_configurations = height(configurations);
-num_spins = width(configurations);
-transposed = configurations';
-inferredCouplingsMatrix = zeros(num_spins,num_spins);%this will contain the final result
+numConfigurations = height(data);
+numSpins = width(data);
+transposed = data';
+solution = zeros(numSpins,numSpins);%this will contain the final result
 beq=0;%equality constraint to impose that each spin has coupling zero with itself
 
 %disequality constraint to impose that all couplings are non negative
-A=-diag(ones(1,num_spins));
-b=zeros(num_spins,1);
+A=-diag(ones(1,numSpins));
+b=zeros(numSpins,1);
 
-startingPoint=zeros(1,num_spins);
+startingPoint=0.5*ones(1,numSpins);
 options = optimoptions(@fmincon,'Algorithm','sqp','MaxFunEvals',30000);
 
 %cycle that for each spin finds the arg max of the associated
 %log-pseudo-likelihoood
-for i=1:num_spins
+for i=1:numSpins
    %equality constraint to impose that each spin has coupling zero with itself
-   Aeq=zeros(1,num_spins);
+   Aeq=zeros(1,numSpins);
    Aeq(i)=1;
    %minimization
-   f=@(x)pseudoLikelihood(x,configurations,i,num_configurations,transposed);
+   f=@(x)pseudoLikelihood(x,data,i,numConfigurations,transposed);
    x=fmincon(f,startingPoint,A,b,Aeq,beq,[],[],[],options);
-   inferredCouplingsMatrix(i:i,:) = x;%each calling of fmincon fills a single line of "solution" 
-   fprintf("%i/%i\n",i,num_spins);%this is to tell the user where we are in the optimization
+   solution(i,:) = x;%each calling of fmincon fills a single line of "solution" 
+   fprintf("%i/%i\n",i,numSpins);%this is to tell the user where we are in the optimization
 end
--- plotResult.m
+++ plotResult.m
@@ -1,7 +1,7 @@
-plot(inferredCouplings,'mo:','LineWidth',2), xlabel('place in the array "inferredCouplings"'), ylabel('value'),title('A descent sort of the effective couplings greater than cutoff'), grid on;
+plot(couplings,'mo:','LineWidth',2), xlabel('place in the array "couplings"'), ylabel('value'),title('A descent sort of the effective couplings greater than cutoff'), grid on;
 savefig('plot.fig');
 close
-histogram(inferredCouplings,50), xlabel('value'), ylabel('number of occurrences in the array "inferredCouplings"'), grid on, title('Histogram of the effective couplings greater than cutoff');
+histogram(couplings,50), xlabel('value'), ylabel('number of occurrences in the array "couplings"'), grid on, title('Histogram of the effective couplings greater than cutoff');
 savefig('hist.fig');
 close
 figure1 = openfig('plot.fig');
