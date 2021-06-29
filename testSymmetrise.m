b0a7ec40970fc80ae77e9aa51e888f2f88a56ae9
Federico Donat <federico.donat@studio.unibo.it>
2021-06-29 17:14:53 +0200

    Updated testSymmetrise.m; added empty input test and random number case

--- testSymmetrise.m
+++ testSymmetrise.m
@@ -1,23 +1,27 @@
-%% test correct result 1
-matrix1 = 0;
-result = symmetrise(matrix1);
-assert( isequal(result,matrix1) );
+%% test correct result zero scalar
+matrix = 0;
+result = symmetrise(matrix);
+assert( isequal(result,matrix) );
 
-%% test correct result 2
-matrix2 = zeros(2,2);
-result = symmetrise(matrix2);
-assert( isequal(result,matrix2) );
+%% test correct result zero matrix
+matrix = zeros(2,2);
+result = symmetrise(matrix);
+assert( isequal(result,matrix) );
 
-%% test correct result 3
-matrix3 = diag(ones(1,5));
-result = symmetrise(matrix3);
-assert( isequal(result,matrix3) );
+%% test correct result diagonal matrix
+matrix = diag(rand*ones(1,5));
+result = symmetrise(matrix);
+assert( isequal(result,matrix) );
 
-%% test correct result 4
-matrix4 = [[1,2];[1,1]];
-result = symmetrise(matrix4);
+%% test correct result non zero matrix
+matrix = [[1,2];[1,1]];
+result = symmetrise(matrix);
 assert( isequal(result,[[1,1.5];[1.5,1]]) );
 
-% aggiungere un test che verifica gli errori
+%% test correct result empty matrix
+matrix = [];
+result = symmetrise(matrix);
+assert( isequal(result,[]) );
+
 
 
