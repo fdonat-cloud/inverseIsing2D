3eb1d0a107bf2ff63f0bdfb9e9393e479bfb5fbb
Federico Donat <federico.donat@studio.unibo.it>
2021-06-29 17:15:29 +0200

    Updated testApplyCutoff.m; added random numbers cases

--- testApplyCutoff.m
+++ testApplyCutoff.m
@@ -1,44 +1,74 @@
-%% test correct result 1
-cutoff = 1;
-array1 = cutoff*ones(1,2);
-result = applyCutoff(array1,cutoff);
+%% test empty input
+cutoff = rand;
+array = [];
+result = applyCutoff(array,cutoff);
 assert( isequal(result,[]) );
 
-%% test correct result 2
-cutoff = 1;
-array2 = (cutoff+1)*ones(1,2);
-result = applyCutoff(array2,cutoff);
-assert( isequal(result,array2) );
-
-%% test correct result 3
-cutoff = 1;
-array3 = cutoff;
-result = applyCutoff(array3,cutoff);
+%% test elements equal to cutoff
+cutoff = rand;
+array = cutoff*ones(1,2);
+result = applyCutoff(array,cutoff);
 assert( isequal(result,[]) );
 
-%% test correct result 4
-cutoff = 1;
-array4 = cutoff+1;
-result = applyCutoff(array4,cutoff);
-assert( isequal(result,array4) );
+%% test elements greater than cutoff
+cutoff = rand;
+array = (cutoff+1)*ones(1,2);
+result = applyCutoff(array,cutoff);
+assert( isequal(result,array) );
 
-%% test correct result 5
-cutoff = 1;
-array5 = [cutoff+1,cutoff];
-result = applyCutoff(array5,cutoff);
+%% test scalar equal to cutoff
+cutoff = rand;
+array = cutoff;
+result = applyCutoff(array,cutoff);
+assert( isequal(result,[]) );
+
+%% test scalar greater than cutoff
+cutoff = rand;
+array = cutoff+1;
+result = applyCutoff(array,cutoff);
+assert( isequal(result,array) );
+
+%% test element greater than cutoff
+cutoff = rand;
+array = [cutoff+1,cutoff];
+result = applyCutoff(array,cutoff);
 assert( isequal(result,cutoff+1) );
 
-%% test correct result 6
-cutoff = 1;
-array6 = [cutoff+1,cutoff+1];
-result = applyCutoff(array6,cutoff);
-assert( isequal(result,array6) );
+%% test two elements greater than cutoff 
+cutoff = rand;
+array = [cutoff+1,cutoff+1];
+result = applyCutoff(array,cutoff);
+assert( isequal(result,array) );
 
 %% test correct result NaN
-cutoff = 1;
-array7 = NaN(1,5);
-result = applyCutoff(array7,cutoff);
+cutoff = rand;
+array = NaN(1,5);
+result = applyCutoff(array,cutoff);
 assert( isequal(result,[]) );
+
+%% test that in the end there are fewer or same number of elements than in the beginning 1
+cutoff = rand;
+array = rand(10,5);
+result = applyCutoff(array,cutoff);
+in = numel(array);
+fin = numel(result);
+assert( not(fin > in) );
+
+%% test that in the end there are fewer or same number of elements than in the beginning 2
+cutoff = rand;
+array = rand(10,50);
+result = applyCutoff(array,cutoff);
+in = numel(array);
+fin = numel(result);
+assert( not(fin > in) );
+
+%% test that in the end there are fewer or same number of elements than in the beginning 3
+cutoff = rand;
+array = rand(1,500);
+result = applyCutoff(array,cutoff);
+in = numel(array);
+fin = numel(result);
+assert( not(fin > in) );
 
 
 
