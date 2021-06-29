93547bf9cfc0f8674c7efb2b9f111bfe2fa74946
Federico Donat <federico.donat@studio.unibo.it>
2021-06-29 17:25:27 +0200

    updated testApplyCutoff.m; just modified the comments

--- testApplyCutoff.m
+++ testApplyCutoff.m
@@ -1,52 +1,52 @@
-%% test empty input
+%% test 1, empty input
 cutoff = rand;
 array = [];
 result = applyCutoff(array,cutoff);
 assert( isequal(result,[]) );
 
-%% test elements equal to cutoff
+%% test 2, elements equal to cutoff
 cutoff = rand;
 array = cutoff*ones(1,2);
 result = applyCutoff(array,cutoff);
 assert( isequal(result,[]) );
 
-%% test elements greater than cutoff
+%% test 3, elements greater than cutoff
 cutoff = rand;
 array = (cutoff+1)*ones(1,2);
 result = applyCutoff(array,cutoff);
 assert( isequal(result,array) );
 
-%% test scalar equal to cutoff
+%% test 4, scalar equal to cutoff
 cutoff = rand;
 array = cutoff;
 result = applyCutoff(array,cutoff);
 assert( isequal(result,[]) );
 
-%% test scalar greater than cutoff
+%% test 5, scalar greater than cutoff
 cutoff = rand;
 array = cutoff+1;
 result = applyCutoff(array,cutoff);
 assert( isequal(result,array) );
 
-%% test element greater than cutoff
+%% test 6, element greater than cutoff
 cutoff = rand;
 array = [cutoff+1,cutoff];
 result = applyCutoff(array,cutoff);
 assert( isequal(result,cutoff+1) );
 
-%% test two elements greater than cutoff 
+%% test 7, two elements greater than cutoff 
 cutoff = rand;
 array = [cutoff+1,cutoff+1];
 result = applyCutoff(array,cutoff);
 assert( isequal(result,array) );
 
-%% test correct result NaN
+%% test 8, correct result NaN
 cutoff = rand;
 array = NaN(1,5);
 result = applyCutoff(array,cutoff);
 assert( isequal(result,[]) );
 
-%% test that in the end there are fewer or same number of elements than in the beginning 1
+%% test 9, that in the end there are fewer or same number of elements than in the beginning
 cutoff = rand;
 array = rand(10,5);
 result = applyCutoff(array,cutoff);
@@ -54,7 +54,7 @@ in = numel(array);
 fin = numel(result);
 assert( not(fin > in) );
 
-%% test that in the end there are fewer or same number of elements than in the beginning 2
+%% test 10, that in the end there are fewer or same number of elements than in the beginning
 cutoff = rand;
 array = rand(10,50);
 result = applyCutoff(array,cutoff);
@@ -62,7 +62,7 @@ in = numel(array);
 fin = numel(result);
 assert( not(fin > in) );
 
-%% test that in the end there are fewer or same number of elements than in the beginning 3
+%% test 11, that in the end there are fewer or same number of elements than in the beginning
 cutoff = rand;
 array = rand(1,500);
 result = applyCutoff(array,cutoff);
