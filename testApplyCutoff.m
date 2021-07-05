b12f989236c3968bc74feeb5df4881cf4d15887a
Federico Donat <72623354+fdonat-cloud@users.noreply.github.com>
2021-07-05 14:20:04 +0200

    Update testApplyCutoff.m
    Now tests are based more on random numbers

--- testApplyCutoff.m
+++ testApplyCutoff.m
@@ -1,28 +1,34 @@
-%% test 1, empty input
+%% test 1, empty input must return itself
 cutoff = rand;
 array = [];
 result = applyCutoff(array,cutoff);
 assert( isequal(result,[]) );
 
-%% test 2, elements equal to cutoff
+%% test 2, elements equal to cutoff must be deleted
 cutoff = rand;
-array = cutoff*ones(1,2);
+height = randi([1 100],1,1);
+width = randi([1 100],1,1);
+array = cutoff*ones(height,width);
 result = applyCutoff(array,cutoff);
 assert( isequal(result,[]) );
 
-%% test 3, elements greater than cutoff
+%% test 3, elements greater than cutoff must not be deleted
 cutoff = rand;
-array = (cutoff+1)*ones(1,2);
+height = randi([1 100],1,1);
+width = randi([1 100],1,1);
+array = (cutoff+1)*ones(height,width);
 result = applyCutoff(array,cutoff);
-assert( isequal(result,array) );
+fin = numel(result);
+in = numel(array);
+assert( isequal(in,fin) );
 
-%% test 4, scalar equal to cutoff
+%% test 4, scalar = cutoff must be deleted
 cutoff = rand;
 array = cutoff;
 result = applyCutoff(array,cutoff);
 assert( isequal(result,[]) );
 
-%% test 5, scalar greater than cutoff
+%% test 5, scalar > cutoff must be deleted
 cutoff = rand;
 array = cutoff+1;
 result = applyCutoff(array,cutoff);
@@ -40,36 +46,23 @@ array = [cutoff+1,cutoff+1];
 result = applyCutoff(array,cutoff);
 assert( isequal(result,array) );
 
-%% test 8, correct result NaN
+%% test 8, NaNs must be deleted
 cutoff = rand;
-array = NaN(1,5);
+height = randi([1 100],1,1);
+width = randi([1 100],1,1);
+array = NaN(height,width);
 result = applyCutoff(array,cutoff);
 assert( isequal(result,[]) );
 
-%% test 9, that in the end there are fewer or same number of elements than in the beginning
+%% test 9, in the end there must be no more elements than in the beginning
 cutoff = rand;
-array = rand(10,5);
+height = randi([1 100],1,1);
+width = randi([1 100],1,1);
+array = rand(height,width);
 result = applyCutoff(array,cutoff);
 in = numel(array);
 fin = numel(result);
 assert( not(fin > in) );
-
-%% test 10, that in the end there are fewer or same number of elements than in the beginning
-cutoff = rand;
-array = rand(10,50);
-result = applyCutoff(array,cutoff);
-in = numel(array);
-fin = numel(result);
-assert( not(fin > in) );
-
-%% test 11, that in the end there are fewer or same number of elements than in the beginning
-cutoff = rand;
-array = rand(1,500);
-result = applyCutoff(array,cutoff);
-in = numel(array);
-fin = numel(result);
-assert( not(fin > in) );
-
 
 
 
