34fca5760e2120ad21cd21b0ac664f103bb7ff35
Federico Donat <72623354+fdonat-cloud@users.noreply.github.com>
2021-07-05 14:15:54 +0200

    Update testSymmetrise.m
    Now more tests are based on random numbers

--- testSymmetrise.m
+++ testSymmetrise.m
@@ -1,27 +1,59 @@
-%% test correct result zero scalar
+%% test zero scalar must be unchanged
 matrix = 0;
 result = symmetrise(matrix);
 assert( isequal(result,matrix) );
 
-%% test correct result zero matrix
-matrix = zeros(2,2);
+%% test zero matrix must be unchanged
+height = randi([1 100],1,1);
+width = height;
+matrix = zeros(height,width);
 result = symmetrise(matrix);
 assert( isequal(result,matrix) );
 
-%% test correct result diagonal matrix
-matrix = diag(rand*ones(1,5));
+%% test diagonal matrix must be unchanged
+lengthOfDiag = randi([1,100],1,1);
+matrix = diag(rand*ones(1,lengthOfDiag));
 result = symmetrise(matrix);
 assert( isequal(result,matrix) );
 
-%% test correct result non zero matrix
+%% test non zero matrix must be transformed correctly
 matrix = [[1,2];[1,1]];
 result = symmetrise(matrix);
 assert( isequal(result,[[1,1.5];[1.5,1]]) );
 
-%% test correct result empty matrix
+%% test empty matrix must be unchanged
 matrix = [];
 result = symmetrise(matrix);
 assert( isequal(result,[]) );
+
+%% test output and input must have same number of elements
+height = randi([1 100],1,1);
+width = height;
+matrix = rand(height,width);
+result = symmetrise(matrix);
+in = numel(matrix);
+fin = numel(result);
+assert( fin == in );
+
+%% test negative-valued matrix must return negative-valued matrix
+height = randi([1 100],1,1);
+width = height;
+matrix = rand(height,width);
+matrix = matrix - 2;
+result = symmetrise(matrix);
+numFinalNegativeElements = sum(sum(result<0));
+assert( numFinalNegativeElements == numel(matrix) );
+
+%% test positive-valued matrix must return positive-valued matrix
+height = randi([1 100],1,1);
+width = height;
+matrix = rand(height,width);
+matrix = matrix + 0.2;
+result = symmetrise(matrix);
+numFinalPositiveElements = sum(sum(result>0));
+assert( numFinalPositiveElements == numel(matrix) );
+
+
 
 
 
