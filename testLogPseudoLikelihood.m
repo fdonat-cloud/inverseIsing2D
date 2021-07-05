e4f4d2326863f8e67e79b387b62b6b1dadd45bea
Federico Donat <72623354+fdonat-cloud@users.noreply.github.com>
2021-07-05 15:43:36 +0200

    Update testLogPseudoLikelihood.m
    Added a test for non positivity with broad coverage

--- testLogPseudoLikelihood.m
+++ testLogPseudoLikelihood.m
@@ -1,4 +1,58 @@
-%% test function is non positive with minimal data
+%% test non positivity
+% since the function is the log a probability, it must never be > 0
+for couplingsValue = -20:20
+    for numConfigurations = 1:10
+        for numSpins = 1:10
+            data = ones(numConfigurations,numSpins);
+            couplings = couplingsValue*ones(1,numSpins);
+            for spinIndex = 1:numSpins
+                value = logPseudoLikelihood(couplings,data,spinIndex);
+                assert( not (value > 0 ) );
+            end
+        end
+    end
+end
+
+%% test additivity
+% the log-PL of the union of data must be the sum of the log-PLs
+numSpins = randi([1 200],1,1);
+numConfigurations1 = randi([1 5000],1,1);
+numConfigurations2 = randi([1 5000],1,1);
+couplings = rand(1,numSpins);
+data1 = rand(numConfigurations1,numSpins);
+data2 = rand(numConfigurations2,numSpins);
+data = [data1;data2];
+spinIndex = randi([1 numSpins],1,1);
+numConfigurations = height(data);
+value1 = numConfigurations1 * logPseudoLikelihood(couplings,data1,spinIndex);
+value2 = numConfigurations2 * logPseudoLikelihood(couplings,data2,spinIndex);
+value = numConfigurations * logPseudoLikelihood(couplings,data,spinIndex);
+tolerance = 1E-4;
+assert( abs(value-value1-value2) < tolerance );
+
+%% test additivity 2
+% the log-PL of the union of data must be the sum of the log-PLs
+for numConfigurations1 = 1:10
+    for numConfigurations2 = 1:10
+        for numSpins = 1:100
+            couplings = rand(1,numSpins);
+            data1 = rand(numConfigurations1,numSpins);
+            data2 = rand(numConfigurations2,numSpins);
+            data = [data1;data2];
+            numConfigurations = height(data);
+            numConfigurations = height(data);
+            for spinIndex = 1:numSpins
+                value1 = numConfigurations1 * logPseudoLikelihood(couplings,data1,spinIndex);
+                value2 = numConfigurations2 * logPseudoLikelihood(couplings,data2,spinIndex);
+                value = numConfigurations * logPseudoLikelihood(couplings,data,spinIndex);
+                tolerance = 1E-10;
+                assert( abs(value-value1-value2) < tolerance );
+            end
+        end
+    end
+end
+
+%% test non positivity with minimal data
 couplings = rand;
 data = [1];
 spinIndex = 1;
@@ -41,42 +95,3 @@ data = ones(numConfigurations,numSpins);
 spinIndex = randi([1 numSpins],1,1);
 value = logPseudoLikelihood(couplings,data,spinIndex);
 assert( not (value > 0 ) );
-
-%% test additivity
-% the log-PL of the union of data must be the sum of the log-PLs
-numSpins = randi([1 200],1,1);
-numConfigurations1 = randi([1 5000],1,1);
-numConfigurations2 = randi([1 5000],1,1);
-couplings = rand(1,numSpins);
-data1 = rand(numConfigurations1,numSpins);
-data2 = rand(numConfigurations2,numSpins);
-data = [data1;data2];
-spinIndex = randi([1 numSpins],1,1);
-numConfigurations = height(data);
-value1 = numConfigurations1 * logPseudoLikelihood(couplings,data1,spinIndex);
-value2 = numConfigurations2 * logPseudoLikelihood(couplings,data2,spinIndex);
-value = numConfigurations * logPseudoLikelihood(couplings,data,spinIndex);
-tolerance = 1E-4;
-assert( abs(value-value1-value2) < tolerance );
-
-%% test additivity 2
-% the log-PL of the union of data must be the sum of the log-PLs
-for numConfigurations1 = 1:10
-    for numConfigurations2 = 1:10
-        for numSpins = 1:100
-            couplings = rand(1,numSpins);
-            data1 = rand(numConfigurations1,numSpins);
-            data2 = rand(numConfigurations2,numSpins);
-            data = [data1;data2];
-            numConfigurations = height(data);
-            numConfigurations = height(data);
-            for spinIndex = 1:numSpins
-                value1 = numConfigurations1 * logPseudoLikelihood(couplings,data1,spinIndex);
-                value2 = numConfigurations2 * logPseudoLikelihood(couplings,data2,spinIndex);
-                value = numConfigurations * logPseudoLikelihood(couplings,data,spinIndex);
-                tolerance = 1E-10;
-                assert( abs(value-value1-value2) < tolerance );
-            end
-        end
-    end
-end
