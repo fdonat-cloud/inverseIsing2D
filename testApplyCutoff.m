97938827c16d1819a90d8d47e0403e515d4d17db
Federico Donat <72623354+fdonat-cloud@users.noreply.github.com>
2021-07-06 14:35:36 +0200

    Update testApplyCutoff.m
    Deleted useless tests, broadened coverage of some tests

--- testApplyCutoff.m
+++ testApplyCutoff.m
@@ -1,70 +1,82 @@
-%% test 1, empty input must return itself
-cutoff = rand;
-array = [];
-result = applyCutoff(array,cutoff);
-assert( isequal(result,[]) );
+%% test in the end there must be no more elements than in the beginning
+rangeCutoff = -5:5;
+rangeHeight = 0:10;
+rangeWidth = 0:15;
 
-%% test 2, elements equal to cutoff must be deleted
-cutoff = rand;
-height = randi([1 100],1,1);
-width = randi([1 100],1,1);
-array = cutoff*ones(height,width);
-result = applyCutoff(array,cutoff);
-assert( isequal(result,[]) );
+for cutoff = rangeCutoff
+    for value = (cutoff-10):(cutoff+10)
+        for height = rangeHeight
+            for width = rangeWidth
+                matrix = value * ones(height,width);
+                output = applyCutoff(matrix,cutoff);
+                in = numel(matrix);
+                fin = numel(output);
+                assert( not(fin > in) );
+            end
+        end
+    end
+end
 
-%% test 3, elements greater than cutoff must not be deleted
-cutoff = rand;
-height = randi([1 100],1,1);
-width = randi([1 100],1,1);
-array = (cutoff+1)*ones(height,width);
-result = applyCutoff(array,cutoff);
-fin = numel(result);
-in = numel(array);
-assert( isequal(in,fin) );
+%% test matrix with elements greater than cutoff must be unchanged
+rangeCutoff = -5:5;
+rangeHeight = 0:10;
+rangeWidth = 0:15;
 
-%% test 4, scalar = cutoff must be deleted
-cutoff = rand;
-array = cutoff;
-result = applyCutoff(array,cutoff);
-assert( isequal(result,[]) );
+for cutoff = rangeCutoff
+    for value = (cutoff+0.1):(cutoff + 10)
+        for height = rangeHeight
+            for width = rangeWidth
+                matrix = value * ones(height,width);
+                output = applyCutoff(matrix,cutoff);
+                in = numel(matrix);
+                fin = numel(output);
+                assert( fin == in );
+            end
+        end
+    end
+end
 
-%% test 5, scalar > cutoff must be deleted
-cutoff = rand;
-array = cutoff+1;
-result = applyCutoff(array,cutoff);
-assert( isequal(result,array) );
+%% test matrix with elements minor than or equal to cutoff must be nullified
+rangeCutoff = -5:5;
+rangeHeight = 0:10;
+rangeWidth = 0:10;
 
-%% test 6, element greater than cutoff
-cutoff = rand;
-array = [cutoff+1,cutoff];
-result = applyCutoff(array,cutoff);
-assert( isequal(result,cutoff+1) );
+for cutoff = rangeCutoff
+    for value = (cutoff-10):cutoff
+        for height = rangeHeight
+            for width = rangeWidth
+                matrix = value * ones(height,width);
+                output = applyCutoff(matrix,cutoff);
+                fin = numel(output);
+                assert( fin == 0 );
+            end
+        end
+    end
+end
 
-%% test 7, two elements greater than cutoff 
-cutoff = rand;
-array = [cutoff+1,cutoff+1];
-result = applyCutoff(array,cutoff);
-assert( isequal(result,array) );
+%% test empty input must return itself
+rangeCutoff = -5:5;
 
-%% test 8, NaNs must be deleted
-cutoff = rand;
-height = randi([1 100],1,1);
-width = randi([1 100],1,1);
-array = NaN(height,width);
-result = applyCutoff(array,cutoff);
-assert( isequal(result,[]) );
+for cutoff = rangeCutoff
+    matrix = [];
+    output = applyCutoff(matrix,cutoff);
+    assert( isequal(output,matrix) );
+end
 
-%% test 9, in the end there must be no more elements than in the beginning
-cutoff = rand;
-height = randi([1 100],1,1);
-width = randi([1 100],1,1);
-array = rand(height,width);
-result = applyCutoff(array,cutoff);
-in = numel(array);
-fin = numel(result);
-assert( not(fin > in) );
+%% test NaNs must be deleted
+rangeCutoff = -100:100;
+rangeHeight = 0:10;
+rangeWidth = 0:10;
 
-
+for cutoff = rangeCutoff
+    for height = rangeHeight
+        for width = rangeWidth
+            matrix = NaN(height,width);
+            output = applyCutoff(matrix,cutoff);
+            assert( isequal(output,[]) );
+        end
+    end
+end
 
 
 
