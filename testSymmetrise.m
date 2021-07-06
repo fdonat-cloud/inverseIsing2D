784ac0aa46742923b4e2c086d37726b79a22f528
Federico Donat <72623354+fdonat-cloud@users.noreply.github.com>
2021-07-06 14:33:27 +0200

    Update testSymmetrise.m
    Deleted useless tests, added variables, changed names to variables, broadened coverage of some tests

--- testSymmetrise.m
+++ testSymmetrise.m
@@ -1,66 +1,58 @@
-%% test positive(negative)-valued matrix must return positive(negative)-valued matrix
+%% test output and input must have same number of elements
+rangeHeight = 0:10;
+
 for var = 0:1
     sign = -1 + 2 * var;%-1 or +1
-    for height = 1:100
+    for height = rangeHeight
         width = height;
-        matrix = ones(height,width);
-        result = symmetrise(matrix);
-        numFinalPositiveElements = sum(sum(result>0));
-        assert( numFinalPositiveElements == numel(matrix) );
+        input = sign * ones(height,width);
+        output = symmetrise(input);
+        in = numel(input);
+        fin = numel(output);
+        assert( in == fin );
     end
 end
 
-%% test zero scalar must be unchanged
-matrix = 0;
-result = symmetrise(matrix);
-assert( isequal(result,matrix) );
+%% test positive-valued matrix must return positive-valued matrix
+rangeHeight = 0:10;
 
-%% test zero matrix must be unchanged
-height = randi([1 100],1,1);
-width = height;
-matrix = zeros(height,width);
-result = symmetrise(matrix);
-assert( isequal(result,matrix) );
-
-%% test diagonal matrix must be unchanged
-lengthOfDiag = randi([1,100],1,1);
-matrix = diag(rand*ones(1,lengthOfDiag));
-result = symmetrise(matrix);
-assert( isequal(result,matrix) );
-
-%% test non zero matrix must be transformed correctly
-matrix = [[1,2];[1,1]];
-result = symmetrise(matrix);
-assert( isequal(result,[[1,1.5];[1.5,1]]) );
-
-%% test empty matrix must be unchanged
-matrix = [];
-result = symmetrise(matrix);
-assert( isequal(result,[]) );
-
-%% test output and input must have same number of elements
-height = randi([1 100],1,1);
-width = height;
-matrix = rand(height,width);
-result = symmetrise(matrix);
-in = numel(matrix);
-fin = numel(result);
-assert( fin == in );
+for height = rangeHeight
+    width = height;
+    input = ones(height,width);
+    output = symmetrise(input);
+    numFinalPositiveElements = sum(sum(output > 0)); 
+    assert( numFinalPositiveElements == numel(input) );
+end
 
 %% test negative-valued matrix must return negative-valued matrix
-height = randi([1 100],1,1);
-width = height;
-matrix = rand(height,width);
-matrix = matrix - 2;
-result = symmetrise(matrix);
-numFinalNegativeElements = sum(sum(result<0));
-assert( numFinalNegativeElements == numel(matrix) );
+rangeHeight = 0:10;
 
-%% test positive-valued matrix must return positive-valued matrix
-height = randi([1 100],1,1);
-width = height;
-matrix = rand(height,width);
-matrix = matrix + 0.2;
-result = symmetrise(matrix);
-numFinalPositiveElements = sum(sum(result>0));
-assert( numFinalPositiveElements == numel(matrix) );
+for height = rangeHeight
+    width = height;
+    input = -ones(height,width);
+    output = symmetrise(input);
+    numFinalPositiveElements = sum(sum(output < 0)); 
+    assert( numFinalPositiveElements == numel(input) );
+end
+
+%% test diagonal matrix must be unchanged
+rangeDiagLength = 0:10;
+rangeValues = -10:10;
+
+for diagLength = rangeDiagLength
+    for value = rangeValues
+        input = diag(value * ones(1,diagLength));
+        output = symmetrise(input);
+        assert( isequal(output,input) );
+    end
+end
+
+%% test correct result
+input = [[1,2];[1,1]];
+output = symmetrise(input);
+assert( isequal(output,[[1,1.5];[1.5,1]]) );
+
+%% test empty matrix must be unchanged
+input = [];
+output = symmetrise(input);
+assert( isequal(output,[]) );
