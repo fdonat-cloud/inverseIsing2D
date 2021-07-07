94c2e4233282b00269fb474b4d1bf803dbe874a4
Federico Donat <72623354+fdonat-cloud@users.noreply.github.com>
2021-07-07 09:50:17 +0200

    Update logPseudoLikelihood.m
    Added documentation and errors

--- logPseudoLikelihood.m
+++ logPseudoLikelihood.m
@@ -1,25 +1,56 @@
-function f = logPseudoLikelihood(couplings,configurations,spin_index)
-%LOGPSEUDOLIKELIHOOD returns the log-pseudo-likelihood
-%   Parameters
-%    ----------
-%    couplings : 1 x num_spins array
-%    configurations : num_configurations x num_spins array
-%    spin_index : scalar
+function f = logPseudoLikelihood(couplings,configurations,spinIndex)
+%LOGPSEUDOLIKELIHOOD of the couplings between one spin and all the others
+%   Parameters:
+%    
+%    couplings : 1 x numSpins array
+%    configurations : num_configurations x numSpins array
+%    spinIndex : positive integer
+%
+%   Returns:
+%    
+%    The log of the pseudo-likelihood divided by the number of
+%    configurations
+%
+%   Raises:
+%    
+%    Error if the third parameter is not a positive integer
+%    Error if the first parameter has not the same number of elements of 
+%    the width of the second parameter
+%    Error if any parameter is empty
 %
 %   All it requires is the effective couplings, the whole data 
 %   ('configurations') and the index of the spin 
-%   The argument 'couplings' is the 1 x num_spins variable to be varied in
+%   The argument 'couplings' is the 1 x numSpins variable to be varied in
 %   order to find the (only) critical point
+%
+%   For references about the theory, see https://arxiv.org/abs/1702.01522
+
+numConfigurations = height(configurations);
+numSpins = width(configurations);
+f=0;
+
+if not( spinIndex == round(spinIndex) ) 
+    error('third parameter must be a integer')
+end
+
+if not(spinIndex > 0)
+    error('third parameter must be positive')
+end
+
+if numel(spinIndex) == 0 || numel(couplings) == 0 || numel(configurations) == 0
+    error('all three parameter must not be empty')
+end
+
+if not( numel(couplings) == numSpins )
+    error('first parameter must have width(configurations) number of elements')
+end
+
+for mu=1:numConfigurations
+    scalarProduct = dot(couplings,configurations(mu,:));
+    spin = configurations(mu,spinIndex);
+    f = f - log(1 + exp(-2*spin*scalarProduct) );
+end
    
-   num_configurations = height(configurations);
-   f=0;
-   
-   for mu=1:num_configurations
-      scalarProduct = dot(couplings,configurations(mu,:));
-      spin = configurations(mu,spin_index);
-      f = f - log(1 + exp(-2*spin*scalarProduct) );
-   end
-   
-   f = f / num_configurations;
+f = f / numConfigurations;
    
 end
