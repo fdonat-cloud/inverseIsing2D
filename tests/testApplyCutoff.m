584534e0b3351950c862c76708f16556418ae9bb
Federico Donat <72623354+fdonat-cloud@users.noreply.github.com>
2021-07-09 14:59:23 +0200

    Update testApplyCutoff.m
    Added a test for single NaN elimination

--- tests/testApplyCutoff.m
+++ tests/testApplyCutoff.m
@@ -1,7 +1,8 @@
-%% test in the end there must be no more elements than in the beginning
+%% test: in the end there must be no more elements than in the beginning
+   %this is a general property that must be true for any cutoff and input
 rangeCutoff = -5:5;
 rangeHeight = 0:10;
-rangeWidth = 0:15;
+rangeWidth = 0:10;
 
 for cutoff = rangeCutoff
     for value = (cutoff-10):(cutoff+10)
@@ -17,7 +18,8 @@ for cutoff = rangeCutoff
     end
 end
 
-%% test matrix with elements greater than cutoff must be unchanged
+%% test: matrix with elements greater than cutoff must be unchanged
+   %this must be true in the specific case of values greater than cutoff
 rangeCutoff = -5:5;
 rangeHeight = 0:10;
 rangeWidth = 0:15;
@@ -37,6 +39,7 @@ for cutoff = rangeCutoff
 end
 
 %% test matrix with elements minor than or equal to cutoff must be nullified
+   %this must be true in the specific case of values not greater than cutoff
 rangeCutoff = -5:5;
 rangeHeight = 0:10;
 rangeWidth = 0:10;
@@ -78,5 +81,25 @@ for cutoff = rangeCutoff
     end
 end
 
+%% test single NaN must be deleted
+rangeCutoff = -5:5;
+rangeHeight = 1:10;
+rangeWidth = 1:10;
 
+for cutoff = rangeCutoff
+    for height = rangeHeight
+        for width = rangeWidth
+            matrix = (cutoff + 1) * ones(height,width);
+            for nanRow = 1:height
+                for nanCol = 1:width
+                matrix(nanRow,nanCol) = NaN;
+                output = applyCutoff(matrix,cutoff);
+                assert( sum(output == output) == numel(output) );
+
+                matrix(nanRow,nanCol) = cutoff + 1;
+                end
+            end
+        end
+    end
+end
 
