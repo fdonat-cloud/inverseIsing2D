62a9ba4046c52d9af77589d927433fd8262611d1
Federico Donat <72623354+fdonat-cloud@users.noreply.github.com>
2021-07-09 17:57:40 +0200

    Update analyze.m
    Transformed into function

--- analyze.m
+++ analyze.m
@@ -1,26 +1,49 @@
-load('cutoff.mat');
-symSolution = symmetrize(solution);
-difference = solution - symSolution;
-relativeAsymmetricity = norm(difference) / norm(symSolution);
-couplings = zeros(numSpins,numSpins);
+function couplings = analyzeIsing(argMax)
+%ANALYZEISING symmetrizes the input and makes it a cut off and sorted vector
+%   Parameters
+%
+%    argMax: a numerical square matrix
+%
+%   Returns
+%
+%    couplings: 1 x M numerical matrix
+%
+%   Raises
+%
+%    Error if the input is not a square matrix
+%    Error if the input is not numeric
+%
+%   The input should be the output of the function infer.m
 
-%cycle in order to lose redundant info contained in symSolution
+if not( width(argMax) == height(argMax) )
+    error('input must be a square matrix')
+end
+
+if not( isnumeric(argMax) )
+    error('imput must be a numeric matrix')
+end
+
+numSpins = width(argMax);
+couplings = zeros(numSpins,numSpins);
+addpath(genpath('otherFunctions'));
+load('cutoff.mat');
+symArgMax = symmetrize(argMax);
+difference = argMax - symArgMax;
+relativeAsymmetricity = norm(difference) / norm(symArgMax);
+
+   %cycle in order to lose redundant info contained in symArgMax
 for j = 1:numSpins
     for i = 1:numSpins
         if not(i > j)
-            couplings(i,j) = symSolution(i,j);
+            couplings(i,j) = symArgMax(i,j);
         end
     end
 end
 
 couplings = applyCutoff(couplings,cutoff);
-%in order to make it communicative when plotted
+   %in order to make it communicative when plotted
 couplings = sort(couplings,"descend");
 
-numberOfCouplings = numel(couplings);
-
-save analysis.mat relativeAsymmetricity numberOfCouplings solution symSolution
+save analysis.mat relativeAsymmetricity argMax symArgMax
 movefile('analysis.mat','results');
-
-
-
+end
