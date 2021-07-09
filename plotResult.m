e9f1d451f5a3e860a1b2fa03cf8372869d489dd9
Federico Donat <72623354+fdonat-cloud@users.noreply.github.com>
2021-07-09 17:59:26 +0200

    Update plotResult.m
    Transformed into function

--- plotResult.m
+++ plotResult.m
@@ -1,3 +1,18 @@
+function uselessOutput = plotResultIsing(couplings)
+%PLOTRESULTISING plots a histogram and a plot and saves them in 'results'
+%   Parameters
+%
+%    couplings: a numerical 1 x M array
+%
+%   Returns
+%
+%    uselessOutput: an empty variable with no meaning
+%
+%   The input should be the output of the function analyze.m
+
+uselessOutput = [];
+addpath(genpath('otherFunctions'));
+
 f = figure('visible', 'off');
 plot(couplings,'mo:','LineWidth',2), xlabel('place in the array "couplings"'), ylabel('value'),title('A descent sort of the effective couplings greater than cutoff'), grid on;
 print -djpeg sorting.jpg
@@ -10,3 +25,5 @@ close(g)
 
 movefile('sorting.jpg','results');
 movefile('histogram.jpg','results');
+
+end
